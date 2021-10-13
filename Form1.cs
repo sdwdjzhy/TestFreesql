590b2c041f90efb1382c9874eba0e4234dbffaec
姜志远 <sdwdjzhy@qq.com>
2021-10-13 10:05:59 +0800

    添加项目文件。

---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using FreeSql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestFreesql
+{
+    public partial class Form1 : Form
+    {
+        IFreeSql Fsql;
+        public Form1()
+        {
+            InitializeComponent();
+
+            var connectionString = "data source=(local);initial catalog=TestFreesql;user id=sa;password=123456;MultipleActiveResultSets=True;Min pool size=1";
+            Fsql = new FreeSqlBuilder()
+                .UseConnectionString(DataType.SqlServer, connectionString)
+                .UseAutoSyncStructure(false)
+                .Build();
+            Fsql.Aop.CurdAfter += (s, e) =>
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(e.Sql);
+                Console.WriteLine("".PadRight(20, '*'));
+
+                Console.ResetColor();
+            };
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var list = Fsql.Select<tTest>().ToList(i => new tTestVM
+            {
+                Id = i.Id,
+                Name = i.Name,
+            });
+
+            MessageBox.Show("Test");
+        }
+    }
+}
---
+++ tTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestFreesql
+{
+    public class tTest
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public AppEnum.Type1 Type { get; set; }
+    }
+
+
+    public class tTestVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public AppEnum.Type2 Type
+        {
+            get
+            {
+                if (Name == "成功")
+                {
+                    return AppEnum.Type2.成功;
+                }
+                else
+                {
+                    return AppEnum.Type2.失败;
+                }
+            }
+        }
+    }
+    public class AppEnum
+    {
+        public enum Type1
+        {
+            已成功,
+            已失败,
+        }
+        public enum Type2
+        {
+            成功,
+            失败,
+        }
+    }
+}
